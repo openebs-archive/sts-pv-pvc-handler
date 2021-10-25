3d897aa26de9428ef0c2a31b8c5f1291166ff7fb
kumarsaurabhraj <kumarsaurabhraj.sr@gmail.com>
2021-10-26 02:31:56 +0530

    Filter openebs PVCs

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ksraj123/lister-sa
 go 1.16
 
 require (
+	k8s.io/api v0.22.2
 	k8s.io/apimachinery v0.22.2
 	k8s.io/client-go v0.22.2
 )
--- main.go
+++ main.go
@@ -2,69 +2,58 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	kubeconfig := flag.String("kubeconfig", "/home/vivek/.kube/config", "location to your kubeconfig file")
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+
+	openEbsStorageClassesNames := [3]string{"openebs-device", "openebs-hostpath"}
+
+	config, err := rest.InClusterConfig()
 	if err != nil {
-		// handle error
-		fmt.Printf("erorr %s building config from flags\n", err.Error())
-		config, err = rest.InClusterConfig()
-		if err != nil {
-			fmt.Printf("error %s, getting inclusterconfig", err.Error())
-		}
+		fmt.Printf("error %s, getting inclusterconfig", err.Error())
 	}
+
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		// handle error
 		fmt.Printf("error %s, creating clientset\n", err.Error())
 	}
+
 	ctx := context.Background()
 
-	pods, err := clientset.CoreV1().Pods("default").List(ctx, metav1.ListOptions{})
-	if err != nil {
-		// handle error
-		fmt.Printf("error %s, while listing all the pods from default namespace\n", err.Error())
-	}
-	fmt.Println("Pods from default namespace")
-	for _, pod := range pods.Items {
-		fmt.Println(pod)
+	allPvcs, errPVC := clientset.CoreV1().PersistentVolumeClaims("default").List(ctx, metav1.ListOptions{})
+
+	if errPVC != nil {
+		fmt.Printf("error %s, getting PVCs\n", err.Error())
 	}
 
-	// pvcs, err := clientset.CoreV1().PersistentVolumeClaims("default").List(ctx, metav1.ListOptions{})
-	// for _, pvc := range pvcs.Items {
-	// 	pvcdetails, err := clientset.CoreV1().PersistentVolumeClaims("default").Get(ctx, pvc.Name, metav1.GetOptions{})
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 	} else {
-	// 		fmt.Println(pvcdetails.ManagedFields)
-	// 		fmt.Println(pvcdetails.OwnerReferences)
-	// 		fmt.Println(pvcdetails.ObjectMeta)
-	// 		fmt.Println(pvcdetails.Status)
-	// 		pvcdetails.eve
-	// 		fmt.Printf("\n")
-	// 		fmt.Printf("\n")
-	// 		fmt.Printf("\n")
-	// 		fmt.Printf("\n")
-	// 	}
-	// }
+	var openebsPvcs []*v1.PersistentVolumeClaim
 
-	// fmt.Println(pvcs)
+	for _, pvc := range allPvcs.Items {
+		pvcdetails, err := clientset.CoreV1().PersistentVolumeClaims("default").Get(ctx, pvc.Name, metav1.GetOptions{})
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			pvcStorageClassName := *pvcdetails.Spec.StorageClassName
+			for _, openEbsStorageClass := range openEbsStorageClassesNames {
+				if pvcStorageClassName == openEbsStorageClass {
+					openebsPvcs = append(openebsPvcs, pvcdetails)
+				}
 
-	// fmt.Println("Deployments are")
-	// deployments, err := clientset.AppsV1().Deployments("default").List(ctx, metav1.ListOptions{})
-	// if err != nil {
-	// 	fmt.Printf("listing deployments %s\n", err.Error())
-	// }
-	// for _, d := range deployments.Items {
-	// 	fmt.Printf("%s", d.Name)
-	// }
+			}
+		}
+	}
+
+	fmt.Println("Printing Filtered PVCs")
+	for _, openebsPVC := range openebsPvcs {
+		fmt.Println(openebsPVC.ObjectMeta.Name)
+		fmt.Println(openebsPVC.ObjectMeta.Labels)
+	}
+
 }
