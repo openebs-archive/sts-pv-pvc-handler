ba0768b7efdc7f91f12b10d069f0d0b5e5a7c719
kumarsaurabhraj <kumarsaurabhraj.sr@gmail.com>
2021-11-25 05:14:21 +0530

    unit testing complete
    Signed-off-by: Kumar Saurabh Raj <kumarsaurabhraj.sr@gmail.com>

---
+++ pkg/constants/constants.go
@@ -0,0 +1,9 @@
+package constants
+
+const (
+	TEST_NAMESPACE           = "test"
+	NAMESPACES_ENV_VAR       = "NAMESPACES"
+	PROVISIONERS_ENV_VAR     = "PROVISIONERS"
+	STORAGE_CLASS_ANNOTATION = "openebs.io/delete-dangling-pvc"
+	STS_PVC_SELECTOR         = "sts-pvc-selector"
+)
