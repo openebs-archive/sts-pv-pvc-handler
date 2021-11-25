df08396b5e3cec1bf170021bb5bf3eda8daa46b9
kumarsaurabhraj <kumarsaurabhraj.sr@gmail.com>
2021-11-25 09:40:28 +0530

    Integration tests and Job
    Signed-off-by: Kumar Saurabh Raj <kumarsaurabhraj.sr@gmail.com>

--- tests/suite_test.go
+++ tests/suite_test.go
@@ -1,52 +1,89 @@
 package tests
 
 import (
-	"flag"
-	"os"
+	"context"
+	"fmt"
+	"strings"
 	"testing"
+	"time"
 
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	"github.com/ksraj123/lister-sa/tests/generators"
+	"github.com/ksraj123/lister-sa/tests/setup"
+	CoreV1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
+	envtest "sigs.k8s.io/controller-runtime/pkg/envtest"
 )
 
-var (
-	kubeConfigPath                  string
-	openebsNamespace                string
-	clientset                       *kubernetes.Clientset
-	LocalPVProvisionerLabelSelector = "openebs.io/component-name=openebs-localpv-provisioner"
-)
-
-func init() {
-	flag.StringVar(&kubeConfigPath, "kubeconfig", os.Getenv("KUBECONFIG"), "path to kubeconfig to invoke kubernetes API calls")
-	flag.StringVar(&openebsNamespace, "openebs-namespace", "openebs", "kubernetes namespace where the OpenEBS components are present")
-	config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+func startCluster() (*kubernetes.Clientset, *envtest.Environment) {
+	testenv := &envtest.Environment{}
+	cfg, err := testenv.Start()
 	if err != nil {
 		panic(err.Error())
 	}
-	clientset, err = kubernetes.NewForConfig(config)
+	clientset, err := kubernetes.NewForConfig(cfg)
 	if err != nil {
 		panic(err.Error())
 	}
+	return clientset, testenv
 }
 
-func TestSource(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Test application deployment")
+func stopCluster(testenv *envtest.Environment) {
+	testenv.Stop()
 }
 
-var _ = BeforeSuite(func() {
-	By("waiting for openebs-localpv-provisioner pod to come into running state")
-	provPodCount := GetPodRunningCountEventually(
-		openebsNamespace,
-		LocalPVProvisionerLabelSelector,
-		1,
-		clientset,
-	)
-	Expect(provPodCount).To(Equal(1))
+func TestMain(t *testing.T) {
+	ctx := context.Background()
+	clientSet, clusterTestEnv := startCluster()
+	defer stopCluster(clusterTestEnv)
 
-	// run our job
-})
+	// Ensure openebs-localpv-provisioner pod running
+	setup.TestOpenEbsPodRunningState(t, clientSet, ctx, 90)
+	setup.CreateStorageClass(t, clientSet, ctx, 10)
+	statefulsetName := "test-sts"
+	setup.CreateStatefulSet(t, clientSet, ctx, statefulsetName, 10)
 
-var _ = Describe("Shopping cart", func() {})
+	// Delete the statefulset
+
+	err := clientSet.AppsV1().StatefulSets("default").Delete(ctx, statefulsetName, metav1.DeleteOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	time.Sleep(5 * time.Second)
+
+	// now the statefulset PVCs are in dangling state
+
+	serviceAccountName := "openebs-maya-operator"
+	image := "ksraj123/stale-sts-pvc-cleaner:0.1"
+	env := []CoreV1.EnvVar{
+		{
+			Name:  "PROVISIONERS",
+			Value: "openebs.io/local",
+		},
+		{
+			Name:  "NAMESPACES",
+			Value: "default",
+		},
+	}
+
+	job := generators.GenerateJob("test-job", map[string]string{"jobGroup": "test"}, serviceAccountName, image, env)
+	_, err = clientSet.BatchV1().Jobs("default").Create(ctx, job, metav1.CreateOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	time.Sleep(5 * time.Second)
+
+	pvcs, err := clientSet.CoreV1().PersistentVolumeClaims("default").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		fmt.Printf("error %s, getting PVCs\n", err.Error())
+	}
+	for _, pvc := range pvcs.Items {
+		if strings.Contains(pvc.Name, statefulsetName) {
+			t.Fatalf("Dangling PVCs %v not deleted by Job", pvc.Name)
+		}
+	}
+
+	// clientSet.BatchV1().Jobs("default").Delete(ctx, "test-job", metav1.DeleteOptions{})
+}
